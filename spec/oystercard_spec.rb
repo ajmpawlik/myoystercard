563db3a984007376959da2cb602d8088128a67c7
Aleksandra <ajmpawlik@gmail.com>
2019-06-07 15:51:42 +0100

    three types of respond_to matcher working

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,15 @@
+require 'oystercard'
+
 describe Oystercard do
+  it { is_expected.to respond_to(balance) }
+
+  it 'second option'do
+    expect(subject).to respond_to(balance)
+  end
+
+  it 'third option' do
+    oystercard = Oystercard.new
+    expect(oystercard).to respond_to(balance)
+  end
+
 end
